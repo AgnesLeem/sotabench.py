23d6ae7c51245dfc5cce76ed115921f4ff9fc2f5
AgnesLee <51048931+AgnesLeem@users.noreply.github.com>
2020-03-13 17:11:02 +0800

    sotabench.py
    from sotabencheval.image_classification import ImageNetEvaluator
    evaluator = ImageNetEvaluator(
                 model_name='ResNeXt-101-32x8d',
                 paper_arxiv_id='1611.05431')

---
+++ sotabench.py
@@ -0,0 +1,5 @@
+from sotabencheval.image_classification import ImageNetEvaluator
+
+evaluator = ImageNetEvaluator(
+             model_name='ResNeXt-101-32x8d',
+             paper_arxiv_id='1611.05431')
